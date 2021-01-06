53f90bea9824da8c805c736d9432d5f6f5e992b1
Italo Alves <italo_silva007@hotmail.com>
2021-01-06 20:59:38 -0300

    Changing routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,7 +8,6 @@ import LoginTeacher from './pages/TeacherSignIn/index'
 import StudentSignUp from './pages/StudentSignUp/index'
 import TeacherSignUp from './pages/TeacherSignUp/index'
 import Courses from './pages/Courses'
-import test from './pages/test'
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -30,7 +29,6 @@ const Routes = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/teste" component={test} />
         <Route path="/auth/login" component={Login} />
         <Route path="/auth/loginTeacher" component={LoginTeacher} />
         <Route exact path="/signup/student" component={StudentSignUp} />
