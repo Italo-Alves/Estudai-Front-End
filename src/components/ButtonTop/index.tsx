d107a905f53427b6861d54418e20e5b1c69efd57
Italo Alves <italo_silva007@hotmail.com>
2021-02-18 20:42:38 -0300

    Refactoring with styled components

--- src/components/ButtonTop/index.tsx
+++ src/components/ButtonTop/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import * as FaIcons from 'react-icons/fa'
 
-import './buttonTop.css'
-import './script.jsx'
+import { Button } from './styles'
 
 const ButtonTop: React.FC = () => {
+  useEffect(() => {
+    const button = document.getElementById('toTopButton')
+    const rootElement = document.documentElement
+    const target = document.querySelector('footer')
+
+    function callback(entries: IntersectionObserverEntry[]) {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        if (entry.isIntersecting) {
+          console.log(entry.isIntersecting)
+          button?.classList.add('show')
+        } else {
+          button?.classList.remove('show')
+        }
+      })
+    }
+
+    function scrollToTop() {
+      rootElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }
+
+    button?.addEventListener('click', scrollToTop)
+
+    let observer = new IntersectionObserver(callback)
+
+    target && observer.observe(target)
+  }, [])
   return (
-    <button id="toTopButton" className="mr-4">
+    <Button>
       <FaIcons.FaAngleUp />
-    </button>
+    </Button>
   )
 }
 
